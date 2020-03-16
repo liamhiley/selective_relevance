e6de4ea9d50587760ded90b5491b6583025ffe8a
Liam Hiley <liamhiley@live.co.uk>
2020-03-16 16:09:40 +0000

    Added gui file for drawing on canvas

---
+++ gui.py
@@ -0,0 +1,88 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QPainter, QPalette, QColor, QPixmap
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.canvas = Canvas()
+
+        w = QWidget()
+        l = QVBoxLayout()
+        w.setLayout(l)
+        l.addWidget(self.canvas)
+
+        palette = QHBoxLayout()
+        self.add_palette_buttons(palette)
+        l.addLayout(palette)
+
+        self.setCentralWidget(w)
+
+class Canvas(QLabel):
+    # https://www.learnpyqt.com/courses/custom-widgets/bitmap-graphics/
+    def __init__(self):
+        super().__init__()
+        pixmap = QPixmap(600, 300)
+        self.setPixmap(pixmap)
+
+        self.last_x, self.last_y = None, None
+        self.pen_color = QColor('#000000')
+
+    def set_pen_color(self, c):
+        self.pen_color = QColor(c)
+
+    def mouseMoveEvent(self, e):
+        if self.last_x is None: # First event.
+            self.last_x = e.x()
+            self.last_y = e.y()
+            return # Ignore the first time.
+
+        painter = QPainter(self.pixmap())
+        p = painter.pen()
+        p.setWidth(4)
+        p.setColor(self.pen_color)
+        painter.setPen(p)
+        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+        painter.end()
+        self.update()
+
+        # Update the origin for next time.
+        self.last_x = e.x()
+        self.last_y = e.y()
+
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
+
+
+
+def set_dark_fusion(qApp):
+    #https://gist.github.com/lschmierer/443b8e21ad93e2a2d7eb#file-dark_fusion-py
+    qApp.setStyle("Fusion")
+
+    dark_palette = QPalette()
+
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+
+    qApp.setPalette(dark_palette)
+
+    qApp.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
