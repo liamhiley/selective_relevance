74a949b1d0a62651b0c7489ad460671606c6b528
Liam Hiley <liamhiley@live.co.uk>
2021-06-03 14:42:20 +0100

    Added fusion model to models

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+__all__ = [
+    "c3d",
+    "resnet3d",
+    "mars",
+    "slowfast",
+    "s3dg",
+    "fusion"
+]
+
+from . import c3d
+from . import resnet3d
+from . import mars
+from . import slowfast
+from . import s3dg
+from . import fusion
